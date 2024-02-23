def5e2abc624b22c713cbfcb58d21aaead864615
ricardomrcruz <strasbourgwebsolutions@gmail.com>
2024-02-23 13:59:26 +0100

    imageinputfix

--- frontend/src/pages/admin/newProduct.tsx
+++ frontend/src/pages/admin/newProduct.tsx
@@ -3,9 +3,7 @@ import { useCreateProductMutation } from "@/graphql/generated/schema";
 import { useRouter } from "next/router";
 import { FormEvent, useState } from "react";
 
-
 export default function NewProduct() {
-
   const [createAd] = useCreateProductMutation();
 
   const [imageURL, setImageURL] = useState("");
@@ -14,84 +12,87 @@ export default function NewProduct() {
     e.preventDefault();
     const formData = new FormData(e.target as HTMLFormElement);
     const formJSON: any = Object.fromEntries(formData.entries());
-    formJSON.price = parseFloat(formJSON.price)
-  }
-
-
+    formJSON.price = parseFloat(formJSON.price);
+  };
 
   return (
     <AdminLayout title="Admin - TGC">
-      
       <div className="w-6/12 mt-20 ml-20 p-10 bg-[#161B21] bg-opacity-90 backdrop-filter backdrop-blur-lg rounded-md border border-gray-700 shadow-2xl">
-      <h2 className="text-lg text-gray-200 mb-6">Add New  Product</h2>
+        <h2 className="text-lg text-gray-200 mb-6">Add New Product</h2>
 
-      
-      
-      <form  className="space-y-4">
-      <div>
-        <label htmlFor="name" className="block  font-medium text-md text-gray-200">Name</label>
-        <input
-          required
-          type="text"
-          name="name"
-          id="name"
-          placeholder="Playstation 3"
-          className="mt-1 block w-full px-3 py-2 border text-gray-200 border-gray-700 bg-[#090B0D] rounded-md shadow-sm focus:outline-none focus:ring-indigo-500 focus:border-purple-700"
-        />
+        <form onSubmit={handleSubmit} className="space-y-4">
+          <div>
+            <label
+              htmlFor="name"
+              className="block  font-medium text-md text-gray-200"
+            >
+              Name
+            </label>
+            <input
+              required
+              type="text"
+              name="name"
+              id="name"
+              placeholder="Playstation 3"
+              className="mt-1 block w-full px-3 py-2 border text-gray-200 border-gray-700 bg-[#090B0D] rounded-md shadow-sm focus:outline-none focus:ring-indigo-500 focus:border-purple-700"
+            />
+          </div>
+          <div>
+            <label
+              htmlFor="description"
+              className="block  font-medium text-md text-gray-200"
+            >
+              Description
+            </label>
+            <textarea
+              required
+              rows={7}
+              name="description"
+              id="description"
+              placeholder="Playstation 3"
+              className="mt-1 block w-full px-3 py-2 border text-gray-200 border-gray-700 bg-[#090B0D] rounded-md shadow-sm focus:outline-none focus:ring-indigo-500 focus:border-purple-700"
+            ></textarea>
+          </div>
+          <div>
+            <label
+              htmlFor="price"
+              className="block  font-medium text-md text-gray-200"
+            >
+              Price
+            </label>
+            <input
+              required
+              type="number"
+              name="price"
+              id="price"
+              min={0}
+              placeholder="349,99"
+              className="mt-1 block w-full px-3 py-2 border text-gray-200 border-gray-700 bg-[#090B0D] rounded-md shadow-sm focus:outline-none focus:ring-indigo-500 focus:border-purple-700"
+            />
+          </div>
+          <div>
+            <label
+              htmlFor="imageUrl"
+              className="block  font-medium text-md text-gray-200"
+            >
+              Image URL
+            </label>
+            <input
+              type="text"
+              name="picture"
+              id="picture"
+              placeholder="https://imageshack.com/zoot.png"
+              className="mt-1 block w-full px-3 py-2 border  border-gray-700 bg-[#090B0D] rounded-md shadow-sm focus:outline-none focus:ring-indigo-500 focus:border-purple-700"
+            />
+          </div>
+          <button
+            type="submit"
+            className="py-2 px-4 bg-black text-white font-semibold rounded-lg shadow-md hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-opacity-75"
+          >
+            Add Product
+          </button>
+        </form>
       </div>
-      <div>
-        <label htmlFor="description" className="block  font-medium text-md text-gray-200">Description</label>
-        <textarea
-          required
-          rows={7}
-          name="description"
-          id="description"
-          placeholder="Playstation 3"
-          className="mt-1 block w-full px-3 py-2 border text-gray-200 border-gray-700 bg-[#090B0D] rounded-md shadow-sm focus:outline-none focus:ring-indigo-500 focus:border-purple-700"
-        ></textarea>
-      </div>
-      <div>
-        <label htmlFor="price" className="block  font-medium text-md text-gray-200">Price</label>
-        <input
-          required
-          type="number"
-          name="price"
-          id="price"
-          min={0}
-          placeholder="349,99"
-          className="mt-1 block w-full px-3 py-2 border text-gray-200 border-gray-700 bg-[#090B0D] rounded-md shadow-sm focus:outline-none focus:ring-indigo-500 focus:border-purple-700"
-        />
-      </div>
-      <div>
-        <label htmlFor="imageUrl" className="block  font-medium text-md text-gray-200">Image URL</label>
-        <input
-          type="text"
-          name="picture"
-          id="picture"
-          placeholder="https://imageshack.com/zoot.png" 
-          value={imageURl}
-          onChange={(e) => setImageURl(e.target.value)}
-         
-          className="mt-1 block w-full px-3 py-2 border  border-gray-700 bg-[#090B0D] rounded-md shadow-sm focus:outline-none focus:ring-indigo-500 focus:border-purple-700"
-        />
-        {/* <input 
-        type="file"
-        onChange={(e) => {
-          if(e.target.files?.[0])
-          uploadFile(e.target.files?.[0]).then(setImageURL)
-        }} 
-        /> */}
-      </div>
-      <button type="submit" className="py-2 px-4 bg-black text-white font-semibold rounded-lg shadow-md hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-opacity-75">
-        Add Product
-      </button>
-    </form>
-    </div>
-    
-    
     </AdminLayout>
   );
 }
-
-
-
