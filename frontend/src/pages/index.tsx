0fb92c3cb3ab221f28cef1a1a44d85dc7c17cfe9
ricardomrcruz <strasbourgwebsolutions@gmail.com>
2024-03-09 23:19:09 +0100

    indexprod

--- frontend/src/pages/index.tsx
+++ frontend/src/pages/index.tsx
@@ -4,12 +4,23 @@ import React from "react";
 export default function Home() {
   return (
     <Layout pageTitle="Wildmazon">
-      <div className="text-white text-3xl  w-full text-center justify-center m-10">
+      <div className="text-white text-3xl  w-full text-center justify-center my-10">
         <h1 className="">Products</h1>
       </div>
-      <div className="flex w-full items-center">
+      <div className="  items-center grid grid-cols-4">
        
-        <div className="inter-var w-1/4 bg-gray-50 relative dark:hover:shadow-2xl dark:hover:shadow-emerald-500/[0.1] dark:bg-black dark:border-white/[0.2] border-black/[0.1]  h-[25rem] rounded-sm p-8 border">
+      <div className="inter-var w-full bg-gray-50 relative dark:hover:shadow-2xl dark:hover:shadow-emerald-500/[0.1] dark:bg-black dark:border-white/[0.2] border-black/[0.1]  h-[25rem] rounded-sm p-5 border">
+          <div className="text-xl font-bold text-neutral-600 dark:text-white">
+           Product
+          </div>
+          <p className="text-neutral-500 text-sm max-w-sm mt-2 dark:text-neutral-300">
+            {}% CBD • 1% Max THC
+          </p>
+          <div className="flex-1 max-h-[87%] w-full overflow-hidden relative mt-4 rounded-sm">
+             
+          </div>
+        </div>
+        <div className="inter-var w-full bg-gray-50 relative dark:hover:shadow-2xl dark:hover:shadow-emerald-500/[0.1] dark:bg-black dark:border-white/[0.2] border-black/[0.1]  h-[25rem] rounded-sm p-8 border">
           <div className="text-2xl font-bold text-neutral-600 dark:text-white">
            Product
           </div>
@@ -20,6 +31,68 @@ export default function Home() {
              
           </div>
         </div>
+        <div className="inter-var w-full bg-gray-50 relative dark:hover:shadow-2xl dark:hover:shadow-emerald-500/[0.1] dark:bg-black dark:border-white/[0.2] border-black/[0.1]  h-[25rem] rounded-sm p-8 border">
+          <div className="text-2xl font-bold text-neutral-600 dark:text-white">
+           Product
+          </div>
+          <p className="text-neutral-500 text-sm max-w-sm mt-2 dark:text-neutral-300">
+            {}% CBD • 1% Max THC
+          </p>
+          <div className="flex-1 max-h-[87%] w-full overflow-hidden relative mt-4 rounded-sm">
+             
+          </div>
+        </div><div className="inter-var w-full bg-gray-50 relative dark:hover:shadow-2xl dark:hover:shadow-emerald-500/[0.1] dark:bg-black dark:border-white/[0.2] border-black/[0.1]  h-[25rem] rounded-sm p-8 border">
+          <div className="text-2xl font-bold text-neutral-600 dark:text-white">
+           Product
+          </div>
+          <p className="text-neutral-500 text-sm max-w-sm mt-2 dark:text-neutral-300">
+            {}% CBD • 1% Max THC
+          </p>
+          <div className="flex-1 max-h-[87%] w-full overflow-hidden relative mt-4 rounded-sm">
+             
+          </div>
+        </div><div className="inter-var w-full bg-gray-50 relative dark:hover:shadow-2xl dark:hover:shadow-emerald-500/[0.1] dark:bg-black dark:border-white/[0.2] border-black/[0.1]  h-[25rem] rounded-sm p-8 border">
+          <div className="text-2xl font-bold text-neutral-600 dark:text-white">
+           Product
+          </div>
+          <p className="text-neutral-500 text-sm max-w-sm mt-2 dark:text-neutral-300">
+            {}% CBD • 1% Max THC
+          </p>
+          <div className="flex-1 max-h-[87%] w-full overflow-hidden relative mt-4 rounded-sm">
+             
+          </div>
+        </div><div className="inter-var w-full bg-gray-50 relative dark:hover:shadow-2xl dark:hover:shadow-emerald-500/[0.1] dark:bg-black dark:border-white/[0.2] border-black/[0.1]  h-[25rem] rounded-sm p-8 border">
+          <div className="text-2xl font-bold text-neutral-600 dark:text-white">
+           Product
+          </div>
+          <p className="text-neutral-500 text-sm max-w-sm mt-2 dark:text-neutral-300">
+            {}% CBD • 1% Max THC
+          </p>
+          <div className="flex-1 max-h-[87%] w-full overflow-hidden relative mt-4 rounded-sm">
+             
+          </div>
+        </div><div className="inter-var w-full bg-gray-50 relative dark:hover:shadow-2xl dark:hover:shadow-emerald-500/[0.1] dark:bg-black dark:border-white/[0.2] border-black/[0.1]  h-[25rem] rounded-sm p-8 border">
+          <div className="text-2xl font-bold text-neutral-600 dark:text-white">
+           Product
+          </div>
+          <p className="text-neutral-500 text-sm max-w-sm mt-2 dark:text-neutral-300">
+            {}% CBD • 1% Max THC
+          </p>
+          <div className="flex-1 max-h-[87%] w-full overflow-hidden relative mt-4 rounded-sm">
+             
+          </div>
+        </div><div className="inter-var w-full bg-gray-50 relative dark:hover:shadow-2xl dark:hover:shadow-emerald-500/[0.1] dark:bg-black dark:border-white/[0.2] border-black/[0.1]  h-[25rem] rounded-sm p-8 border">
+          <div className="text-2xl font-bold text-neutral-600 dark:text-white">
+           Product
+          </div>
+          <p className="text-neutral-500 text-sm max-w-sm mt-2 dark:text-neutral-300">
+            {}% CBD • 1% Max THC
+          </p>
+          <div className="flex-1 max-h-[87%] w-full overflow-hidden relative mt-4 rounded-sm">
+             
+          </div>
+        </div>
+        
       </div>
     </Layout>
   );
